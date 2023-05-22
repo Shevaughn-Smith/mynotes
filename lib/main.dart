696cfaa0d72e4c2d9e3837d55188d4e164e08079
Shevaughn-Smith <shevaughnssmith@gmail.com>
2023-05-22 11:44:31 -0500

    clean up main file and connect views

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -35,16 +36,22 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // final isEmailVerified = user?.emailVerified ?? false;
-            // if (isEmailVerified) {
-            //   return const Text('Done');
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth
+                .instance.currentUser; // Get information on the current user
+            if (user != null) {
+              // Check if user is logged in
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView(); // push verify email view to screen
+              }
+            } else {
+              return const LoginView();
+            }
+            print(user);
+            return const Text('Done');
           default:
-            return const CircularProgressIndicator();
+            return const CircularProgressIndicator(); // Display a circular progress bar
         }
       },
     );
